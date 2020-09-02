351983a93f7df3466cd032f75aec7e767064d34f
MonicaLuca <monica.lucarini28@gmail.com>
2020-09-01 20:58:30 -0400

    added the components and stories for empty, show, form, confirm, status, error

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,11 @@ import InterviewerList from "components/InterviewerList"
 import Appointment from "components/Appointment/index"
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
+import Show from  "components/Appointment/Show"
+import Confirm from  "components/Appointment/Confirm"
+import Status from  "components/Appointment/Status"
+import Error from "components/Appointment/Error"
+import Form from "components/Appointment/Form"
 
 //Stories for Button
 storiesOf("Button", module)
@@ -143,6 +148,41 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  // .add("Appointment with Time", () => <Appointment time="12pm" />);
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => 
+    <Show 
+    interviewer={interviewer}
+    student={'Lydia Miller-Jones'}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")} />)
+  .add("Confirm", () => 
+    <Confirm 
+    interviewer={interviewer}
+    message={"Delete the appointment?"}
+    onCancel={action("onCancel")}
+    onConfirm={action("onConfirm")} />)
+  .add("Deleting", () => <Status message={'Deleting'}/>)
+  .add("Saving", () => <Status message={'Saving'}/>)
+  .add("Error", () => 
+    <Error 
+    message={'Could not delete appointment.'} 
+    onClose={action("onClose")}
+    />)
+  .add("Edit", () => 
+    <Form 
+    name={'Archie Cohen'} 
+    interviewers={interviewers}
+    interviewer={2}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
+  />)
+  .add("Create", () => 
+    <Form 
+    interviewers={interviewers}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
+  />)
+
+
+  
